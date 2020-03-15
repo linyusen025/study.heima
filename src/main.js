e9c66e5cabba569e61e87d95dea20547b2efcd77
linyusen025 <1275966894@qq.com>
2020-03-15 15:33:28 +0800

    完成项目结构搭建

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 导入element-ui
+import ElementUI from 'element-ui';
+import 'element-ui/lib/theme-chalk/index.css';
+Vue.use(ElementUI);
+
 Vue.config.productionTip = false
 
+// 5输入router
+import router from './router/index.js'
 new Vue({
   render: h => h(App),
+  // 5注入
+  router
 }).$mount('#app')
---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+// 2导入
+import VueRouter from 'vue-router'
+// 3导入vue
+import Vue from 'vue'
+// 3注册
+Vue.use(VueRouter)
+// 4实例路由
+const router = new VueRouter({
+    routes:[
+
+    ]
+});
+// 5输出router
+export default router
